3dec802df98940b2e2158c902c768b7bb0e0f06b
ayush <ayrajsingh67@gmail.com>
2022-05-25 17:20:21 +0530

    Git page

--- src/commonent/Navbar.jsx
+++ src/commonent/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props) {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <a className="navbar-brand" href="#">
             {props.title}
           </a>
           <button
@@ -22,12 +22,12 @@ export default function Navbar(props) {
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <a className="nav-link active" aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <a className="nav-link" href="#">
                   Features
                 </a>
               </li>
